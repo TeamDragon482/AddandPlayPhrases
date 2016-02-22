9819ee9f0a729e98a7a5fdf29ed865228687ccef
sspeaks610 <sspeaks610@tamu.edu>
2016-02-22 17:27:24 -0600

    Created slide-out drawer

--- playphrase/src/main/java/dragon/tamu/playphrase/MainActivity.java
+++ playphrase/src/main/java/dragon/tamu/playphrase/MainActivity.java
@@ -1,13 +1,105 @@
 package dragon.tamu.playphrase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView mDrawerList;
+    RelativeLayout mDrawerPane;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    ArrayList<Language> mLanguages = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Adding languages to the pull out list.
+        for(Language l : Language.values())
+        {
+            mLanguages.add(l);
+        }
+
+        // DrawerLayout
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        // Populate the Navigtion Drawer with options
+        mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        LanguageListAdapter adapter = new LanguageListAdapter(this, mLanguages);
+        mDrawerList.setAdapter(adapter);
+
+        // Drawer Item click listeners
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this.getApplicationContext(), "Selected " + mLanguages.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    enum Language{English, French, Arabic, German}
+    class LanguageListAdapter extends BaseAdapter {
+
+        Context mContext;
+        ArrayList<Language> mLanguages;
+
+        public LanguageListAdapter(Context context, ArrayList<Language> langauges)
+        {
+            mContext = context;
+            mLanguages = langauges;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return mLanguages.size();
+        }
+
+        @Override
+        public Object getItem(int position)
+        {
+            return mLanguages.get(position);
+        }
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.drawer_item, null);
+            }
+            else {
+                view = convertView;
+            }
+
+            TextView titleView = (TextView) view.findViewById(R.id.title);
+
+            titleView.setText( mLanguages.get(position).name() );
+
+            return view;
+        }
+    }
+
 }
