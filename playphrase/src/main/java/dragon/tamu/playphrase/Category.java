89d2441c90c2187867277b12aa452d937d401009
sspeaks <sspeaks610@tamu.edu>
2016-03-24 18:53:26 -0500

    Fixed Merge Conflicts

--- playphrase/src/main/java/dragon/tamu/playphrase/Category.java
+++ playphrase/src/main/java/dragon/tamu/playphrase/Category.java
@@ -1,16 +1,29 @@
 package dragon.tamu.playphrase;
 
+<<<<<<< remotes/origin/Seth_Dev
 import com.bignerdranch.expandablerecyclerview.Model.ParentListItem;
 
 import java.util.List;
 
 public class Category implements ParentListItem{
 
+=======
+import java.lang.String;
+import java.lang.Object;
+
+import com.bignerdranch.expandablerecyclerview.Model.ParentListItem;
+
+import java.util.List;
+
+public class Category implements ParentListItem{
+
+>>>>>>> local
     private List<Object> mPhraseList;
     private String mCategoryTitle;
 
     public Category(String mCategoryTitle) {
         this.mCategoryTitle = mCategoryTitle;
+<<<<<<< remotes/origin/Seth_Dev
     }
     public Category(List<Object> phraseList, String title)
     {
@@ -34,6 +47,39 @@ public class Category implements ParentListItem{
     @Override
     public List<?> getChildItemList() {
         return mPhraseList;
+=======
+    }
+    public Category(List<Object> phraseList, String title)
+    {
+        mPhraseList = phraseList;
+        mCategoryTitle = title;
+    }
+
+    public void setChildItemList(List<Object> list)
+    {
+        mPhraseList = list;
+    }
+
+    public String getCategoryTitle() {
+        return mCategoryTitle;
+    }
+
+    public void setCategoryTitle(String mCategoryTitle) {
+        this.mCategoryTitle = mCategoryTitle;
+    }
+
+    @Override
+    public List<?> getChildItemList() {
+        return mPhraseList;
+    }
+
+    @Override
+    public boolean isInitiallyExpanded() {
+        return false;
+    }
+	public void addPhrase(Phrase p){
+        phraseList.add(p);
+>>>>>>> local
     }
 
     @Override
--- playphrase/src/main/java/dragon/tamu/playphrase/Phrase.java
+++ playphrase/src/main/java/dragon/tamu/playphrase/Phrase.java
@@ -4,10 +4,40 @@ public class Phrase
 {
     private String mPhraseText;
 
+<<<<<<< remotes/origin/Seth_Dev
     public Phrase(String text)
     {
         mPhraseText = text;
     }
+
+    public String getPhraseText() {
+        return mPhraseText;
+=======
+public class Phrase
+{
+    private String mPhraseText;
+	public Map<String, String> phraseLanguages; //LangAbrv, fileLocation
+
+    public Phrase(String text)
+    {
+        mPhraseText = text;
+		phraseLanguages = new HashMap<>();
+    }
+	public Phrase(String s, Map<String, String> pL){
+        mPhraseText = s;
+        phraseLanguages.putAll(pL);
+    }
+    public void addLanguage(String langAbrv, String fileLocation){
+        phraseLanguages.put(langAbrv, fileLocation);
+>>>>>>> local
+    }
+
+    public void setPhraseText(String mPhraseText) {
+        this.mPhraseText = mPhraseText;
+    }
+<<<<<<< remotes/origin/Seth_Dev
+}
+=======
 
     public String getPhraseText() {
         return mPhraseText;
@@ -17,3 +47,5 @@ public class Phrase
         this.mPhraseText = mPhraseText;
     }
 }
+
+>>>>>>> local
